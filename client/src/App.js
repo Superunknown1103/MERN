5e8d51a8aac3ea5c19a54ec56110f842f3aae352
Superunknown1103 <superunknown1103@gmail.com>
2018-08-15 22:19:29 -0500

    material UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import Paper from '@material-ui/core/Paper';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import CommentIcon from '@material-ui/icons/Comment';
 
 const TodosQuery =  gql`
 {
@@ -13,15 +21,50 @@ const TodosQuery =  gql`
 `;
 
 class App extends Component {
+    handleToggle = todo => () => {
+     // update todo
+      
+    }
+
   render() {
     const {data: {loading, todos}} = this.props;
     if (loading) {
       return <div>Loading...</div>;
     }
     return (
-    <div>{todos.map(todo => (<div key={`$(todo.id)-todo-item`}>{todo.text}</div>))}</div>
+      <div style={{ display: "flex" }}>
+    <div style={{ margin: "auto", width: 400 }}>
+      <Paper elevation= {1}>
+    <List>
+          {todos.map(todo => (
+            <ListItem
+              key={todo.id}
+              role={undefined}
+              dense
+              button
+              onClick={this.handleToggle(todo)}
+            >
+              <Checkbox
+                checked={todo.complete}
+                tabIndex={-1}
+                disableRipple
+              />
+              <ListItemText primary={todo.text} />
+              <ListItemSecondaryAction>
+                <IconButton aria-label="Comments">
+                  <CommentIcon />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+          ))}
+        </List>
+        </Paper>
+    </div>
+    </div>
     );
   }
 }
+       
+
 
 export default graphql(TodosQuery)(App);
