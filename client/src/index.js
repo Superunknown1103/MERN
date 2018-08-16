4cb9b0b6d90eb8273945a31df11bbba061fc0737
Superunknown1103 <superunknown1103@gmail.com>
2018-08-15 21:37:20 -0500

    added apollo

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { ApolloProvider } from "react-apollo";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ApolloClient from 'apollo-boost';
+
+// the Apollo cache is set up automatically
+const client = new ApolloClient({
+    uri: "http://localhost:4000"
+});
+
+ReactDOM.render(
+    // wrapping the entire application with apollo provider to be able to
+    // access requests to apollo from anywhere in the application
+<ApolloProvider client={client}>
+<App />
+</ApolloProvider>, document.getElementById('root'));
 registerServiceWorker();
