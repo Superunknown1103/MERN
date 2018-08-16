4c370f50ba4bc0b2f0194876e7fba3728618f544
Superunknown1103 <superunknown1103@gmail.com>
2018-08-15 20:31:10 -0500

    added update

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const { GraphQLServer } = require('graphql-yoga');
 const mongoose = require('mongoose');
 
+const Todo = mongoose.model('Todo', {
+  text: String,
+  complete: Boolean
+});
+
 const typeDefs = `
   type Query {
     hello(name: String): String!
@@ -12,24 +17,31 @@ const typeDefs = `
   }
   type Mutation {
     createTodo(text: String!): Todo 
+    updateTodo(id: ID!, complete: Boolean!): Boolean
   }
-`
+
+`;
 
 mongoose.connect('mongodb://localhost/todos');
-
-// ToDo Model
-mongoose.connection.once('open', function() {
-  const Todo = mongoose.model('Todo', {
-    text: String,
-    complete: Boolean
-  })
-});
 
 const resolvers = {
   Query: {
     hello: (_, { name }) => `Hello ${name || 'World'}`,
   },
-}
+  Mutation: {
+    createTodo: async (_, { text }) => {
+      const todo = new Todo({ text, complete: false });
+      await todo.save();
+      return todo;
+    },
+  updateTodo: async (_, {id, complete}) => {
+    await Todo.findByIdAndUpdate(id, {complete});
+    return true;
+  }
+  }
+};
 
 const server = new GraphQLServer({ typeDefs, resolvers })
+mongoose.connection.once('open', function() {
 server.start(() => console.log('Server is running on localhost:4000'))
+});
